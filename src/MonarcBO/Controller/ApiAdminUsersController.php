c248fbc7e3ee5d71832a0ce469e49a1e15ad34da
Guillaume Lesniak <guillaume@netlor.fr>
2016-04-14 11:21:56 +0200

    Users: Add filtering support

--- src/MonarcBO/Controller/ApiAdminUsersController.php
+++ src/MonarcBO/Controller/ApiAdminUsersController.php
@@ -2,12 +2,21 @@
 
 namespace MonarcBO\Controller;
 
+use MonarcBO\Service\UserService;
 use MonarcCore\Controller\AbstractController;
 use Zend\View\Model\JsonModel;
 
 class ApiAdminUsersController extends AbstractController {
     public function getList() {
-        return new JsonModel($this->getService()->getList());
+        $page = $this->params()->fromQuery('page');
+        $limit = $this->params()->fromQuery('limit');
+        $order = $this->params()->fromQuery('order');
+        $filter = $this->params()->fromQuery('filter');
+
+        /** @var UserService $service */
+        $service = $this->getService();
+        return new JsonModel(array('count' => $service->getFilteredCount($page, $limit, $order, $filter),
+            'users' => $service->getList($page, $limit, $order, $filter)));
     }
 
     public function get($id) {
--- src/MonarcBO/Service/UserService.php
+++ src/MonarcBO/Service/UserService.php
@@ -1,6 +1,7 @@
 <?php
 namespace MonarcBO\Service;
 
+use MonarcBO\Model\Table\UserTable;
 use MonarcCore\Service\AbstractService;
 
 class UserService extends AbstractService
@@ -8,9 +9,28 @@ class UserService extends AbstractService
     protected $userTable;
     protected $userEntity;
 
-    public function getList()
+    public function getTotalCount()
     {
-        return $this->get('userTable')->fetchAll();
+        /** @var UserTable $userTable */
+        $userTable = $this->get('userTable');
+        return $userTable->count();
+    }
+
+    public function getFilteredCount($page = 1, $limit = 25, $order = null, $filter = null) {
+        /** @var UserTable $userTable */
+        $userTable = $this->get('userTable');
+
+        return $userTable->countFiltered($page, $limit, $this->parseFrontendOrder($order),
+            $this->parseFrontendFilter($filter, array('firstname', 'lastname', 'email')));
+    }
+
+    public function getList($page = 1, $limit = 25, $order = null, $filter = null)
+    {
+        /** @var UserTable $userTable */
+        $userTable = $this->get('userTable');
+
+        return $userTable->fetchAllFiltered($page, $limit, $this->parseFrontendOrder($order),
+            $this->parseFrontendFilter($filter, array('firstname', 'lastname', 'email')));
     }
 
     public function getEntity($id)

be4b9a7f21cdd6b832b0893e159397f18506680d
Guillaume Lesniak <guillaume@netlor.fr>
2016-04-18 11:41:23 +0200

    Cleanup remaining skeleton layouts

--- config/module.config.php
+++ config/module.config.php
@@ -40,10 +40,7 @@ return array(
             'viewJsonStrategy'
         ),
         'template_map' => array(
-            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
-            'monarc-bo/index/index' => __DIR__ . '/../view/application/index/index.phtml',
-            'error/404'               => __DIR__ . '/../view/error/404.phtml',
-            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+            'monarc-bo/index/index' => __DIR__ . '/../view/layout/layout.phtml',
         ),
         'template_path_stack' => array(
             __DIR__ . '/../view',

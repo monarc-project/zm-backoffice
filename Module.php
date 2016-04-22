e7fed965cd6a273f3e247fa7b409b380e70be1d4
Guillaume Lesniak <guillaume@netlor.fr>
2016-04-22 15:40:36 +0200

    Temporary workaround: Allow home route through RBAC

--- Module.php
+++ Module.php
@@ -171,6 +171,10 @@ class Module
     public function checkRbac(MvcEvent $e) {
         $route = $e->getRouteMatch()->getMatchedRouteName();
 
+        if ($route == 'home') {
+            return true;
+        }
+
         //retrieve connected user
         $sm = $e->getApplication()->getServiceManager();
         $connectedUserService = $sm->get('\MonarcCore\Service\ConnectedUserService');

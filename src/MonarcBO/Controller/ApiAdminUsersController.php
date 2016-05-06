21b930dd145f1c1256f9933f00d4fba5bd2a5e47
Guillaume Lesniak <guillaume@netlor.fr>
2016-05-06 16:37:59 +0200

    APIs Controller: Update to update() method

--- src/MonarcBO/Controller/ApiAdminUsersController.php
+++ src/MonarcBO/Controller/ApiAdminUsersController.php
@@ -43,11 +43,28 @@ class ApiAdminUsersController extends AbstractController
         if (isset($data['dateStart'])) unset($data['dateStart']);
         if (isset($data['dateEnd'])) unset($data['dateEnd']);
 
-        if (isset($data['id']) && $data['id'] > 0) {
-            $service->update($data);
-        } else {
-            $service->create($data);
-        }
+        $service->create($data);
+
+        return new JsonModel(array('status' => 'ok'));
+    }
+
+    public function update($id, $data)
+    {
+        /** @var UserService $service */
+        $service = $this->getService();
+
+        // Security: Don't allow changing role, password, status and history fields. To clean later.
+        if (isset($data['status'])) unset($data['status']);
+        if (isset($data['id'])) unset($data['id']);
+        if (isset($data['salt'])) unset($data['salt']);
+        if (isset($data['updatedAt'])) unset($data['updatedAt']);
+        if (isset($data['updater'])) unset($data['updater']);
+        if (isset($data['createdAt'])) unset($data['createdAt']);
+        if (isset($data['creator'])) unset($data['creator']);
+        if (isset($data['dateStart'])) unset($data['dateStart']);
+        if (isset($data['dateEnd'])) unset($data['dateEnd']);
+
+        $service->update($id, $data);
 
         return new JsonModel(array('status' => 'ok'));
     }

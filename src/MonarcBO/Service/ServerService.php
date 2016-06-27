27adc61eed97ea6164b6aaafa61cefb0d195b7f3
Cyril Rouyer <cyril@netlor.fr>
2016-06-27 07:36:45 +0200

    Fix souci surcharge getFilteredCount & getList)

--- src/MonarcBO/Service/ServerService.php
+++ src/MonarcBO/Service/ServerService.php
@@ -17,7 +17,7 @@ class ServerService extends AbstractService
         return $serverTable->count();
     }
 
-    public function getFilteredCount($page = 1, $limit = 25, $order = null, $filter = null) {
+    public function getFilteredCount($page = 1, $limit = 25, $order = null, $filter = null, $filterAnd = null) {
         /** @var ServerTable $serverTable */
         $serverTable = $this->get('serverTable');
 
@@ -25,8 +25,7 @@ class ServerService extends AbstractService
             $this->parseFrontendFilter($filter, array('label', 'ip_address', 'fqdn')));
     }
 
-    public function getList($page = 1, $limit = 25, $order = null, $filter = null)
-    {
+    public function getList($page = 1, $limit = 25, $order = null, $filter = null, $filterAnd = null) {
         /** @var ServerTable $serverTable */
         $serverTable = $this->get('serverTable');
 

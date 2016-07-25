0c924e4b1b44f8a8c3ac874af1a3d6b25beb4df2
Thomas Metois <thomas@netlor.fr>
2016-07-25 17:59:36 +0200

    Doc Model > change method GET

--- src/MonarcBO/Controller/ApiDocModelsController.php
+++ src/MonarcBO/Controller/ApiDocModelsController.php
@@ -27,6 +27,37 @@ class ApiDocModelsController extends AbstractController
     }
 
     /**
+     * Get list
+     *
+     * @return JsonModel
+     */
+    public function getList()
+    {
+        $page = $this->params()->fromQuery('page');
+        $limit = $this->params()->fromQuery('limit');
+        $order = $this->params()->fromQuery('order');
+        $filter = $this->params()->fromQuery('filter');
+
+        $service = $this->getService();
+
+        $entities = $service->getList($page, $limit, $order, $filter);
+        if (count($this->dependencies)) {
+            foreach ($entities as $key => $entity) {
+                $this->formatDependencies($entities[$key], $this->dependencies);
+            }
+        }
+
+        foreach($entities as $k => $v){
+            $entities[$k]['path'] = './api/docmodels/'.$v['id'];
+        }
+
+        return new JsonModel(array(
+            'count' => $service->getFilteredCount($page, $limit, $order, $filter),
+            $this->name => $entities
+        ));
+    }
+
+    /**
      * Get
      *
      * @param mixed $id
@@ -35,8 +66,26 @@ class ApiDocModelsController extends AbstractController
     public function get($id)
     {
         $entity = $this->getService()->getEntity($id);
-        var_dump($entity);die();
-        return new JsonModel($entity);
+        if(!empty($entity)){
+            $name = pathinfo($entity['path'],PATHINFO_BASENAME);
+            $name = explode('_',$name);
+            unset($name[0]);
+            $name = implode('_',$name);
+
+            $fileContents = file_get_contents($entity['path']);
+            $response = $this->getResponse();
+            $response->setContent($fileContents);
+
+            $headers = $response->getHeaders();
+            $headers->clearHeaders()
+                ->addHeaderLine('Content-Type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+                ->addHeaderLine('Content-Disposition', 'attachment; filename="' . $name . '"')
+                ->addHeaderLine('Content-Length', strlen($fileContents));
+
+            return $this->response;
+        } else {
+            throw new \Exception('Document template not found');
+        }
     }
 
     public function update($id, $data)

eefbe48d198b790f3432356475965a73e00c2982
Guillaume Lesniak <guillaume@netlor.fr>
2016-08-23 10:45:58 +0200

    ScalesTypes: How about actually filtering by ANR id?

--- src/MonarcBO/Controller/ApiScalesTypesController.php
+++ src/MonarcBO/Controller/ApiScalesTypesController.php
@@ -7,7 +7,26 @@ use Zend\View\Model\JsonModel;
 
 class ApiScalesTypesController extends AbstractController
 {
-    protected $dependencies = ['scale'];
+    protected $dependencies = [];
     protected $name = 'types';
+
+    /**
+     * Get List
+     *
+     * @return JsonModel
+     */
+    public function getList()
+    {
+        $order = $this->params()->fromQuery('order');
+        $filter = $this->params()->fromQuery('filter');
+        $anrId = (int) $this->params()->fromRoute('anrId');
+
+        $types = $this->getService()->getList(0, 0, $order, $filter, ['anr' => $anrId]);
+
+        return new JsonModel(array(
+            'count' => count($types),
+            $this->name => $types
+        ));
+    }
 }
 

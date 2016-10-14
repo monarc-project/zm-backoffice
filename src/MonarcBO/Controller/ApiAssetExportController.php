7cdca490ffc21c4005763a2fbd3555368742c999
Thomas Metois <thomas@netlor.fr>
2016-10-14 11:34:58 +0200

    Add Asset Export route/controller

---
+++ src/MonarcBO/Controller/ApiAssetExportController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace MonarcBO\Controller;
+
+use MonarcCore\Controller\AbstractController;
+use Zend\View\Model\JsonModel;
+
+/**
+ * Api Assets Controller
+ *
+ * Class ApiAssetsController
+ * @package MonarcBO\Controller
+ */
+class ApiAssetExportController extends AbstractController
+{
+	public function get($id){
+		$this->methodNotAllowed();
+	}
+
+	public function getList(){
+		$this->methodNotAllowed();
+	}
+	public function create($data){
+        $output = $this->getService()->exportAsset($data);
+
+        $response = $this->getResponse();
+        $response->setContent($output);
+
+        $headers = $response->getHeaders();
+        $headers->clearHeaders()
+            ->addHeaderLine('Content-Type', 'application/binary')
+            ->addHeaderLine('Content-Disposition', 'attachment; filename="' . (empty($data['filename'])?$data['id']:$data['filename']) . '.bin"');
+
+        return $this->response;
+	}
+	public function delete($id){
+		$this->methodNotAllowed($id);
+	}
+	public function deleteList($data){
+		$this->methodNotAllowed();
+	}
+	public function update($id, $data){
+		$this->methodNotAllowed();
+	}
+	public function patch($id, $data){
+		$this->methodNotAllowed();
+	}
+}
---
+++ src/MonarcBO/Controller/ApiAssetExportControllerFactory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace MonarcBO\Controller;
+
+use MonarcCore\Controller\AbstractControllerFactory;
+
+class ApiAssetExportControllerFactory extends AbstractControllerFactory
+{
+    protected $serviceName = '\MonarcCore\Service\AssetService';
+}
+

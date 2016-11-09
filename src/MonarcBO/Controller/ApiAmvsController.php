cb8141b31e87d6147bec51279cfdab18c48d441e
Guillaume Lesniak <guillaume@netlor.fr>
2016-11-09 11:18:08 +0100

    Amvs: Give out implicitPosition and previous on GET

--- src/MonarcBO/Controller/ApiAmvsController.php
+++ src/MonarcBO/Controller/ApiAmvsController.php
@@ -55,5 +55,47 @@ class ApiAmvsController extends AbstractController
             $this->name => $entities
         ));
     }
+
+    /**
+     * Get
+     *
+     * @param mixed $id
+     * @return JsonModel
+     */
+    public function get($id)
+    {
+        $entity = $this->getService()->getEntity($id);
+
+        if (count($this->dependencies)) {
+            $this->formatDependencies($entity, $this->dependencies);
+        }
+
+        // Find out the entity's implicitPosition and previous
+        if ($entity['position'] == 1) {
+            $entity['implicitPosition'] = 1;
+        } else {
+            // We're not at the beginning, get all AMV links of the same asset, and figure out position and previous
+            $amvsAsset = $this->getService()->getList(1, 0, 'position', null, ['asset' => $entity['asset']['id']]);
+
+            $i = 0;
+            foreach ($amvsAsset as $amv) {
+                if ($amv['id'] == $entity['id']) {
+                    if ($i == count($amvsAsset) - 1) {
+                        $entity['implicitPosition'] = 2;
+                    } else {
+                        $entity['implicitPosition'] = 3;
+                        $entity['previous'] = $amvsAsset[$i - 1];
+                        $this->formatDependencies($entity['previous'], $this->dependencies);
+                    }
+
+                    break;
+                }
+
+                ++$i;
+            }
+        }
+
+        return new JsonModel($entity);
+    }
 }
 

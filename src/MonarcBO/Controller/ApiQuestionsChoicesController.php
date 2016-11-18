726d3a51c05e85631bebdd52535193a57f1e7932
Guillaume Lesniak <guillaume@netlor.fr>
2016-11-18 15:05:45 +0100

    ChoicesController: Allow PUT-list

--- src/MonarcBO/Controller/ApiQuestionsChoicesController.php
+++ src/MonarcBO/Controller/ApiQuestionsChoicesController.php
@@ -3,11 +3,46 @@
 namespace MonarcBO\Controller;
 
 use MonarcCore\Controller\AbstractController;
+use MonarcCore\Model\Entity\QuestionChoice;
+use MonarcCore\Model\Table\QuestionChoiceTable;
+use MonarcCore\Service\QuestionChoiceService;
 use Zend\View\Model\JsonModel;
 
 class ApiQuestionsChoicesController extends AbstractController
 {
     protected $dependencies = ['questions'];
     protected $name = 'choices';
+
+    public function replaceList($data) {
+        /** @var QuestionChoiceService $service */
+        $service = $this->getService();
+
+        /** @var QuestionChoiceTable $table */
+        $table = $service->get('table');
+
+        // Remove existing choices
+        $questions = $table->fetchAllFiltered(['id'], 1, 0, null, null, ['question' => $data['questionId']]);
+        foreach ($questions as $q) {
+            $table->delete($q['id']);
+        }
+
+        $question = $this->getService()->get('questionTable')->getEntity($data['questionId']);
+
+        // Add new choices
+        $pos = 1;
+        foreach ($data['choice'] as $c) {
+            $c['position'] = $pos;
+            unset($c['question']);
+
+            /** @var QuestionChoice $choiceEntity */
+            $choiceEntity = new QuestionChoice();
+            $choiceEntity->setQuestion($question);
+            $choiceEntity->exchangeArray($c);
+            $table->save($choiceEntity);
+            ++$pos;
+        }
+
+        return new JsonModel(['status' => 'ok']);
+    }
 }
 

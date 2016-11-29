c929aa385d5810d2a2481dad414c3ae54f5911f9
Guillaume Lesniak <guillaume@netlor.fr>
2016-11-29 09:29:36 +0100

    GuideItems: Fix filtering items by guide

--- src/MonarcBO/Controller/ApiGuidesItemsController.php
+++ src/MonarcBO/Controller/ApiGuidesItemsController.php
@@ -9,5 +9,39 @@ class ApiGuidesItemsController extends AbstractController
 {
     protected $dependencies = ['guide'];
     protected $name = 'items';
+
+    /**
+     * Get list
+     *
+     * @return JsonModel
+     */
+    public function getList()
+    {
+        $page = $this->params()->fromQuery('page');
+        $limit = $this->params()->fromQuery('limit');
+        $order = $this->params()->fromQuery('order');
+        $filter = $this->params()->fromQuery('filter');
+        $guide = $this->params()->fromQuery('guide');
+        if (!is_null($guide)) {
+            $filterAnd = ['guide' => (int) $guide];
+        } else {
+            $filterAnd = [];
+        }
+
+        $service = $this->getService();
+
+        $entities = $service->getList($page, $limit, $order, $filter, $filterAnd);
+        if (count($this->dependencies)) {
+            foreach ($entities as $key => $entity) {
+                $this->formatDependencies($entities[$key], $this->dependencies);
+            }
+        }
+
+        return new JsonModel(array(
+            'count' => $service->getFilteredCount($page, $limit, $order, $filter),
+            $this->name => $entities
+        ));
+    }
+
 }
 

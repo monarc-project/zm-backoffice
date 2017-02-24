c1c123ab7a8fad8863c1b0c6e1ee0eccbc8e145d
Thomas Metois <thomas@netlor.fr>
2017-02-24 14:10:46 +0100

    EVO: Model Table update comments

--- src/MonarcBO/Model/Table/ClientTable.php
+++ src/MonarcBO/Model/Table/ClientTable.php
@@ -1,11 +1,23 @@
 <?php
+/**
+ * @link      https://github.com/CASES-LU for the canonical source repository
+ * @copyright Copyright (c) Cases is a registered trademark of SECURITYMADEIN.LU
+ * @license   MyCases is licensed under the GNU Affero GPL v3 - See license.txt for more information
+ */
 namespace MonarcBO\Model\Table;
 
 use MonarcCore\Model\Table\AbstractEntityTable;
 
+/**
+ * Class ClientTable
+ * @package MonarcBO\Model\Table
+ */
 class ClientTable extends AbstractEntityTable {
+    /**
+     * ClientTable constructor.
+     * @param \MonarcCore\Model\Db $dbService
+     */
     public function __construct(\MonarcCore\Model\Db $dbService) {
         parent::__construct($dbService, '\MonarcBO\Model\Entity\Client');
     }
-
 }
--- src/MonarcBO/Model/Table/ServerTable.php
+++ src/MonarcBO/Model/Table/ServerTable.php
@@ -1,11 +1,23 @@
 <?php
+/**
+ * @link      https://github.com/CASES-LU for the canonical source repository
+ * @copyright Copyright (c) Cases is a registered trademark of SECURITYMADEIN.LU
+ * @license   MyCases is licensed under the GNU Affero GPL v3 - See license.txt for more information
+ */
 namespace MonarcBO\Model\Table;
 
 use MonarcCore\Model\Table\AbstractEntityTable;
 
+/**
+ * Class ServerTable
+ * @package MonarcBO\Model\Table
+ */
 class ServerTable extends AbstractEntityTable {
+    /**
+     * ServerTable constructor.
+     * @param \MonarcCore\Model\Db $dbService
+     */
     public function __construct(\MonarcCore\Model\Db $dbService) {
         parent::__construct($dbService, '\MonarcBO\Model\Entity\Server');
     }
-
 }

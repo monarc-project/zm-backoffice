c1104bf45901677e16850adc7620d2191e2c0d7a
Juan Rocha <juan.rocha@securitymadein.lu>
2019-02-07 11:43:09 +0100

    Fix getList of measure_measure

--- src/MonarcBO/Controller/ApiMeasureMeasureController.php
+++ src/MonarcBO/Controller/ApiMeasureMeasureController.php
@@ -30,19 +30,15 @@ class ApiMeasureMeasureController extends AbstractController
         $limit = $this->params()->fromQuery('limit');
         $order = $this->params()->fromQuery('order');
         $filter = $this->params()->fromQuery('filter');
-      //  $status = $this->params()->fromQuery('status');
         $fatherId = $this->params()->fromQuery('fatherId');
         $childId = $this->params()->fromQuery('childId');
         $filterAnd = [];
-        // if (is_null($status)) {
-        //     $status = 1;
-        // }
-        // $filterAnd = ($status == "all") ? null : ['status' => (int) $status] ;
+
         if ($fatherId) {
-          $filterAnd['father'] = (int) $fatherId;
+          $filterAnd['father'] = $fatherId;
         }
         if ($childId) {
-          $filterAnd['child'] = (int) $childId;
+          $filterAnd['child'] = $childId;
         }
 
         $service = $this->getService();

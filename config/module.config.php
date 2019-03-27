e74b5d5904094a30693968bed40a0f40c60c2273
Jerome Lombardi <jerome.lombardi@cases.lu>
2019-03-27 14:58:03 +0100

    adapt BO's routes to amvs::uuid

--- config/module.config.php
+++ config/module.config.php
@@ -99,7 +99,7 @@ return array(
                 'options' => array(
                     'route' => '/api/amvs[/:id]',
                     'constraints' => array(
-                        'id' => '[0-9]+',
+                        'id' => '[a-f0-9-]*',
                     ),
                     'defaults' => array(
                         'controller' => 'MonarcBO\Controller\ApiAmvs',
